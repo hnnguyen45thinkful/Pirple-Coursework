db34d960dc7c3f30fc373b196473c4c84079afa0
Hieu Nguyen <hnnguyen45@gmail.com>
2019-01-19 17:43:24 -0800

    javascript variables song.js

---
+++ Javascript/song.js
@@ -0,0 +1,56 @@
+/*
+Javascript Homework Assignment #1
+Here is my favorite song
+By: Hieu Nguyen
+
+*/
+
+// This is all the information about my song
+
+// Name of the Music Artist
+var musicArtist = "Linkin Park";
+
+// Name of the Music Album List
+var musicAlbum = "Transformers: Revenge of the Fallen - The Music_Album";
+
+// Here is when the song was released
+var songReleased = 2009;
+
+// Here is when the song was recorded
+var songRecorded = 2008;
+
+// Type of music genre
+var musicGenre = "Rock/Hard Rock";
+
+// Lyric Word Count
+var wordCount = 248;
+
+// Lyric Character Count
+var characterCount = 1252;
+
+/*
+ Time in minutes and seconds EXACTLY minutes:seconds ex. 1:22 means one minute and twenty-two seconds
+ This integer CANNOT be a number or float only string because it is the EXACTLY time finialized.
+*/
+var timeDurationExactly = "4:28"; 
+
+ // Total Number of Seconds ONLY
+var timeDurationSeconds = 268;
+
+// Using the console.log() to display my results for each of the Variables from above.
+
+// Printing out the results on each variables 
+// Printing out to the string for Music Artist, Music Album and Music_Genre
+console.log('Favorite Music Artist/Band: ' + musicArtist)
+console.log('This song came out in the ' + musicAlbum)
+console.log('The song was released in the year ' + songReleased)
+console.log('Otherwise the song was recorded in the year ' + songRecorded)
+console.log('The type of music genre is ' + musicGenre)
+console.log('From counting and listening to the lyrics to all the words counted to ' + wordCount)
+console.log('Also all the characters from the lyrics counted to ' + characterCount)
+console.log('Time Length for my favorite song is ' + timeDurationExactly)
+console.log('Lastly the song is about ' + timeDurationSeconds + ' seconds')
+
+
+
+
