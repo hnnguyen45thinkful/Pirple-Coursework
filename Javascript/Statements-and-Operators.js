891cd883f5d217b5a804924061cbb61968bb40b6
Hieu Nguyen <hnnguyen45@gmail.com>
2019-01-30 02:27:24 -0800

    statement&operators assignment finished extra credit

--- Javascript/Statements-and-Operators.js
+++ Javascript/Statements-and-Operators.js
@@ -33,10 +33,36 @@ if (mensThatAreMortals) {
 } else {
     console.log("All men are not mortal");
 }
-//Same thing from above but I used the includes the mensarrays with "Socrates" to complete the statements give from above. Also if the opposite case (NOT) with all the console.log()s.
+//Same thing from above (if statments and logical operators && (AND)) but I used the includes the mensarrays with "Socrates" to complete the statements give from above. Also if the opposite case (NOT) with all the console.log()s.
 if (mensarrays.includes("Socrates") && mensThatAreMortals) {
     console.log("Socrates is a men");
     console.log("Therefore, socrates is mortal.");
 } else {
     console.log("Socrates is not a men");
 }
+
+//Results on console.log()s are PERFECT!!!.
+
+// All men are mortal
+// Socrates is a men
+// Therefore, socrates is mortal.
+
+
+
+// Extra Credit create an constant object cake with the properties name and  
+const cakeFlavorName = {
+    flavor: "Vanilla",
+    name: "Vanilla cake"
+};
+if (cakeFlavorName.flavor === "Vanilla" || cakeFlavorName.flavor === "Chocolate") {
+    console.log("This cake flavor is either Vanilla or Chocolate.");
+    if (cakeFlavorName.flavor !== "Chocolate") {
+        console.log("This cake flavor is NOT Chocolate.");
+        console.log("Therefore, this cake flavor is Vanilla.");
+    }
+}
+//Results check the same PERFECT!!! on the console.log()s.
+
+// This cake flavor is either Vanilla or Chocolate.
+// This cake flavor is NOT Chocolate.
+// Therefore, this cake flavor is Vanilla.
