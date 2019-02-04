6e284cd8e7dc75cd4517f839dc6c5e5a0f8fcb17
Hieu Nguyen <hnnguyen45@gmail.com>
2019-02-03 23:12:17 -0800

    javascript switch statements time adder

--- Python/loops2.py
+++ Python/loops2.py
@@ -16,7 +16,21 @@ By: Hieu Nguyen
 
 # def gamePlayingBoard(rows, columns)
 """
-Homework Assignment #6
-Advanced Loops
 """
+def draw(rows, coloumns):
+    for x in range(0, rows):
+        if (x % 2 == 0):
+            for y in range(1, coloumns + 1):
+                if (y % 2 == 1):
+                    if (y != coloumns):
+                        print(" ", end='')
+                    else:
+                        print(" ")
+
+                else:
+                    print("|", end='')
+        else:
+            print("------")
+
+draw(10,5)
 
